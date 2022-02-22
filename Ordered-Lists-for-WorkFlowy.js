a910196fc996e4441c716bacc66e12dd2f50671b
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2022-02-22 15:43:19 +0800

    update version number

--- Ordered-Lists-for-WorkFlowy.js
+++ Ordered-Lists-for-WorkFlowy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ordered-Lists-for-WorkFlowy
 // @namespace    https://github.com/BettyJJ
-// @version      0.2.1
+// @version      0.3.1
 // @description  Enable ordered lists for WorkFlowy
 // @author       Betty
 // @match        https://workflowy.com/*
